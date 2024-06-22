66110a44091716814b45de2dcad1d45b41545272
Frankiwi20 <115595456+Frankiwi20@users.noreply.github.com>
2024-06-22 02:59:56 -0700

    Update server.js
    removed sensitive data

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce';
+const PORT = process.env.PORT || ; //add your port
+const MONGO_URI = process.env.MONGO_URI || ''; //Add your path
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
